cd88056eece544ee7e4d7f6b0a684684f0c11e8e
oneat <oneat@vp.pl>
2017-05-08 20:26:41 +0200

    added some text

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -122,9 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '../static/'
 
-#
 
 EMAIL_HOST = 'localhost'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
